bbc2675e98c16f5c45e37f787b73b2a5758a1de1
Moe <66816952+moelak@users.noreply.github.com>
2025-06-14 01:53:30 -0700

    Updated LandingPage.tsx

--- src/components/LandingPage/LandingPage.tsx
+++ src/components/LandingPage/LandingPage.tsx
@@ -223,7 +223,7 @@ const LandingPage = observer(() => {
 />
 
                   <img
-  src="/src/assets/footsteps-1.svg"
+  src="/src/assets/footsteps-1.svg" 
   alt="Footsteps"
   className="absolute   z-0 w-24  opacity-0 top-[148%] right-[130%] transform -translate-x-1/2 translate-y-12 rotate-[-5deg]"
 />
