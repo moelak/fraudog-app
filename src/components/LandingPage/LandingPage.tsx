74d0ab48f9c4d664a9e7f583f1c7687a86b07a46
Moe <66816952+moelak@users.noreply.github.com>
2025-06-14 00:21:09 -0700

    Updated LandingPage.tsx

--- src/components/LandingPage/LandingPage.tsx
+++ src/components/LandingPage/LandingPage.tsx
@@ -176,24 +176,60 @@ const LandingPage = observer(() => {
       src="/src/assets/footsteps-1.svg" 
       alt="Footsteps"
       className="absolute z-0 bottom-32 left-1/2 transform -translate-x-1/2 translate-y-12 w-24 opacity-80"
+         style={{
+                      top: `${75 + idx * 15}%`,
+                      right: `${50 + idx * 8}%`,
+                      transform: `translate(-50%, 0) rotate(${idx % 2 === 0 ? 5 : -5}deg)`,
+                      animationDelay: `${idx * 0.4}s`,
+                      animationDuration: '1s',
+                      animationFillMode: 'forwards',
+                      animationTimingFunction: 'ease-out'
+                    }}
     /> 
 
 <img
       src="/src/assets/footsteps-1.svg" 
       alt="Footsteps"
       className="absolute z-0 top-[75%] right-[50%] transform -translate-x-1/2 translate-y-12 w-24 opacity-80 rotate-[5deg]"
+      style={{
+                      top: `${75 + idx * 15}%`,
+                      right: `${50 + idx * 8}%`,
+                      transform: `translate(-50%, 0) rotate(${idx % 2 === 0 ? 5 : -5}deg)`,
+                      animationDelay: `${idx * 0.4}s`,
+                      animationDuration: '1s',
+                      animationFillMode: 'forwards',
+                      animationTimingFunction: 'ease-out'
+                    }}
     />  
 
    <img
       src="/src/assets/footsteps-1.svg" 
       alt="Footsteps"
       className="absolute z-0 top-[90%] right-[68%] transform -translate-x-1/2 translate-y-12 w-24 opacity-80"
+         style={{
+                      top: `${75 + idx * 15}%`,
+                      right: `${50 + idx * 8}%`,
+                      transform: `translate(-50%, 0) rotate(${idx % 2 === 0 ? 5 : -5}deg)`,
+                      animationDelay: `${idx * 0.4}s`,
+                      animationDuration: '1s',
+                      animationFillMode: 'forwards',
+                      animationTimingFunction: 'ease-out'
+                    }}
     />  
 
   <img
       src="/src/assets/footsteps-1.svg" 
       alt="Footsteps"
       className="absolute z-0 top-[108%] right-[80%] transform -translate-x-1/2 translate-y-12 w-24 opacity-80 rotate-[-5deg]"
+        style={{
+                      top: `${75 + idx * 15}%`,
+                      right: `${50 + idx * 8}%`,
+                      transform: `translate(-50%, 0) rotate(${idx % 2 === 0 ? 5 : -5}deg)`,
+                      animationDelay: `${idx * 0.4}s`,
+                      animationDuration: '1s',
+                      animationFillMode: 'forwards',
+                      animationTimingFunction: 'ease-out'
+                    }}
     />  
         
         
