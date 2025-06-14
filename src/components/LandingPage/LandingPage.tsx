ac9d70c9e8c1982523d31560272b1d8eb1b2f200
Moe <66816952+moelak@users.noreply.github.com>
2025-06-13 22:31:59 -0700

    Updated LandingPage.tsx

--- src/components/LandingPage/LandingPage.tsx
+++ src/components/LandingPage/LandingPage.tsx
@@ -175,7 +175,7 @@ const LandingPage = observer(() => {
                      <img
       src="/src/assets/footsteps.svg"
       alt="Footsteps"
-      className="absolute z-0 bottom-0 left-1/2 transform -translate-x-1/2 translate-y-12 w-24 opacity-80"
+      className="absolute z-0 bottom-4 left-1/2 transform -translate-x-1/2 translate-y-12 w-24 opacity-80"
     />
               {/* Outer Ring */}
 {/* <div className="absolute top-1/2  md:left-[75%] lg:left-[70%] md:w-[540px] md:h-[540px]   lg:w-[640px] lg:h-[640px] -translate-x-1/2 -translate-y-1/2 rounded-full border-2 border-purple-400/10 animate-pulse"></div> */}
