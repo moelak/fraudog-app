d6354bdbc3c4e623f87da14041f6d233050b15df
Moe <66816952+moelak@users.noreply.github.com>
2025-06-14 23:43:04 -0700

    Updated LandingPage.tsx

--- src/components/LandingPage/LandingPage.tsx
+++ src/components/LandingPage/LandingPage.tsx
@@ -223,19 +223,19 @@ const LandingPage = observer(() => {
 <img
   src="/src/assets/footsteps-1.svg"
   alt="Footsteps"
-  className="absolute  footstep z-0 w-16 step-2 opacity-0 top-[75%] right-[50%] transform -translate-x-1/2 translate-y-12 rotate-[5deg]"
+  className="absolute   z-0 w-16 step-2 opacity-0 top-[75%] right-[50%] transform -translate-x-1/2 translate-y-12 rotate-[5deg]"
 /> 
 
 <img
   src="/src/assets/footsteps-1.svg"
   alt="Footsteps" 
-  className="absolute footstep z-0 w-24 step-3 opacity-0 top-[90%] right-[68%] transform -translate-x-1/2 translate-y-12"
+  className="absolute  z-0 w-16 step-3 opacity-0 top-[90%] right-[68%] transform -translate-x-1/2 translate-y-12"
 />
 
 <img
   src="/src/assets/footsteps-1.svg"
   alt="Footsteps"
-  className="absolute  footstep z-0 w-24 step-4 opacity-0 top-[108%] right-[80%] transform -translate-x-1/2 translate-y-12 rotate-[-5deg]"
+  className="absolute   z-0 w-16 step-4 opacity-0 top-[108%] right-[80%] transform -translate-x-1/2 translate-y-12 rotate-[-5deg]"
 />
 
                   
