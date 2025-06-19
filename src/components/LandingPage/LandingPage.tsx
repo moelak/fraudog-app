3f7a586ba1e42f23945307834399da69dc7c60f0
Moe <66816952+moelak@users.noreply.github.com>
2025-06-18 21:46:13 -0700

    Updated LandingPage.tsx

--- src/components/LandingPage/LandingPage.tsx
+++ src/components/LandingPage/LandingPage.tsx
@@ -195,7 +195,7 @@ const LandingPage = observer(() => {
                   
                   {/* Robot Image - 2x Larger (was 320px, now 640px) */}
                   <img 
-                    src="/src/assets/images/robo.png" 
+                    src="/images/robo.png" 
                     alt="AI Fraud Detection Robot" 
                     className="relative z-10 w-[580px] h-[580px] object-contain drop-shadow-2xl"
                     style={{
@@ -204,19 +204,19 @@ const LandingPage = observer(() => {
                   />
 
                   <img
-                    src="/src/assets/footStep.svg"
+                    src="/images/footStep.svg"
                     alt="Footsteps"
                     className="absolute  footstep z-0 w-24 step-2 opacity-0 top-[75%] right-[50%] transform -translate-x-1/2 translate-y-12 rotate-[5deg]"
                   /> 
 
                   <img
-                    src="/src/assets/footStep.svg"
+                    src="/images/footStep.svg"
                     alt="Footsteps" 
                     className="absolute footstep z-0 w-24 step-3 opacity-0 top-[90%] right-[68%] transform -translate-x-1/2 translate-y-12"
                   />
 
                   <img
-                    src="/src/assets/footStep.svg"
+                    src="/images/footStep.svg"
                     alt="Footsteps"
                     className="absolute  footstep z-0 w-24 step-4 opacity-0 top-[108%] right-[80%] transform -translate-x-1/2 translate-y-12 rotate-[-5deg]"
                   />
@@ -295,7 +295,7 @@ const LandingPage = observer(() => {
             ))}
           </div>
           <img
-            src="/src/assets/footStep.svg" 
+            src="/images/footStep.svg" 
             alt="Footsteps"
             className=" hidden lg:flex absolute footstep  step-5 z-0 top-[95%] right-[45%] transform -translate-x-1/2 translate-y-12 w-24 opacity-80 
                      " 
@@ -362,7 +362,7 @@ const LandingPage = observer(() => {
             ))}
           </div>
           <img
-            src="/src/assets/footStep.svg" 
+            src="/images/footStep.svg" 
             alt="Footsteps"
             className="hidden lg:flex absolute footstep  step-5 z-0 top-[90%] right-[45%] transform -translate-x-1/2 translate-y-12 w-24 opacity-80 rotate-[-40deg]"
           />  
@@ -449,7 +449,7 @@ const LandingPage = observer(() => {
             </div>
           </div>
           <img
-            src="/src/assets/footStep.svg" 
+            src="/images/footStep.svg" 
             alt="Footsteps"
             className="hidden lg:flex absolute footstep  step-5 z-0 top-[90%] right-[45%] transform -translate-x-1/2 translate-y-12 w-24 opacity-80 rotate-[-50deg]"
           />  
