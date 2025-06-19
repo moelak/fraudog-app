28078066ffa92f661b0b62f0531a4e12b9fc51ec
Moe <66816952+moelak@users.noreply.github.com>
2025-06-18 23:18:40 -0700

    Updated AuthModal.tsx

--- src/components/Auth/AuthModal.tsx
+++ src/components/Auth/AuthModal.tsx
@@ -117,7 +117,7 @@ console.log("window.location.origin", window.location.origin)
       const { error } = await supabase.auth.signInWithOAuth({
         provider: 'google',
         options: {
-          redirectTo: `${window.location.origin}/auth/callback`,
+          redirectTo: `${window.location.origin}/dashboard`,
         },
       });
 
