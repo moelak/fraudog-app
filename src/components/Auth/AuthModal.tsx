359a0539081440f106f187b42994967840e2fea3
Moe <66816952+moelak@users.noreply.github.com>
2025-06-18 23:00:44 -0700

    Updated AuthModal.tsx

--- src/components/Auth/AuthModal.tsx
+++ src/components/Auth/AuthModal.tsx
@@ -117,7 +117,7 @@ console.log("window.location.origin", window.location.origin)
       const { error } = await supabase.auth.signInWithOAuth({
         provider: 'google',
         options: {
-          redirectTo: `${window.location.origin}/dashboard`,
+          redirectTo: `https://loquacious-manatee-264cad.netlify.app/dashboard`,
         },
       });
 
