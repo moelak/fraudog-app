1905189219c74dbb473932bcf1a33d1ab6e4eaad
Moe <66816952+moelak@users.noreply.github.com>
2025-06-21 08:42:36 -0700

    Updated RuleManagement.tsx

--- src/components/RuleManagement/RuleManagement.tsx
+++ src/components/RuleManagement/RuleManagement.tsx
@@ -117,7 +117,7 @@ const RuleManagement = observer(() => {
         </div>
 
         {/* Rules Table - Removed max-height and overflow to prevent scrolling issues */}
-        <div className="overflow-x-auto">
+        <div className="overflow-x-clip">
           <table className="min-w-full divide-y divide-gray-200">
             <thead className="bg-gray-50">
               <tr>
