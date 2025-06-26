c312a247a4aa252d494f7dc392867e28c0fbaf1a
Moe <66816952+moelak@users.noreply.github.com>
2025-06-25 22:58:51 -0700

    Updated RealtimeTest.tsx

--- src/components/Debug/RealtimeTest.tsx
+++ src/components/Debug/RealtimeTest.tsx
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
 import { supabase } from '../../lib/supabase';
 
-export default function RealtimeTest() {
+export default function RealtimeTest() { 
   useEffect(() => {
     const test = async () => {
       const {
